1cb9965c5af88fc18e44b600b950caff8313b9c7
gevorgakopyan <88122544+gevorgakopyan@users.noreply.github.com>
2021-10-04 09:40:52 -0700

    Time: 12 ms (59.04%), Space: 6 MB (30.98%) - LeetHub

---
+++ palindrome-number/palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if((x<0) || (x%10 == 0 && x!=0))
+            return false;
+        int reverse = 0;
+        while(x > reverse)
+        {
+            reverse = x%10 + reverse * 10;
+            x/=10;
+        }
+        
+        return x == reverse || x == reverse/10;
+    }
+};
