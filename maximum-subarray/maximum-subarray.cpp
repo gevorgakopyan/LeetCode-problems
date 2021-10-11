ca23b98ab8d412d2beb2afbe47524580fc32befe
gevorgakopyan <88122544+gevorgakopyan@users.noreply.github.com>
2021-10-11 09:12:54 -0700

    Time: 163 ms (7.62%), Space: 67.8 MB (18.11%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr = nums[0];
+        int maxSub = nums[0];
+        for(int i = 1; i < nums.size(); i++)
+        {
+            int num = nums[i];
+            curr = max(num, curr+ num);
+            maxSub = max(curr, maxSub);
+        }
+        return maxSub;
+    }
+};
