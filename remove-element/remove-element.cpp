c952d9477a2d15a1a1aa8c4504f60dc36c984906
gevorgakopyan <88122544+gevorgakopyan@users.noreply.github.com>
2021-10-11 08:35:33 -0700

    Time: 0 ms (100.00%), Space: 8.7 MB (92.91%) - LeetHub

---
+++ remove-element/remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j =0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums.at(i) != val)
+            {
+                nums.at(j) = nums.at(i);
+                j++;
+            }
+        }
+        return j;
+    }
+};
