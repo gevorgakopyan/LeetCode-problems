a341addbece4dec50bd75512297507b9906d1f52
gevorgakopyan <88122544+gevorgakopyan@users.noreply.github.com>
2021-11-25 13:47:06 -0800

    Time: 220 ms (16.85%), Space: 85 MB (83.71%) - LeetHub

---
+++ daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int,int>> st;
+        vector<int> res(temperatures.size(), 0);
+        for(int i = temperatures.size() - 1; i >= 0; i--)
+        {
+            while(!st.empty() && st.top().first <= temperatures[i])
+                st.pop();
+            if(!st.empty())
+                res[i] = st.top().second - i;
+            st.push({temperatures[i],i});
+        }
+        return res;
+    }
+};
