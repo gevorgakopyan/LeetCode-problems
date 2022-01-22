72e236841d93ce173f0566f51f815382da03a5ac
gevorgakopyan <88122544+gevorgakopyan@users.noreply.github.com>
2022-01-22 11:12:38 -0800

    Time: 30 ms (39.89%), Space: 14.1 MB (46.15%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> catalog(numCourses);
+        vector<int> prereq(numCourses, 0);
+        for(auto edge : prerequisites)
+        {
+            int a = edge[0];
+            int b = edge[1];
+            catalog[b].push_back(a);
+            prereq[a]++;
+        }
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(prereq[i] == 0)
+            {                
+                q.push(i);
+                prereq[i]--;
+            }
+            
+                
+        }
+        int count = 0;
+        while(!q.empty())
+        {
+            int course = q.front();
+            q.pop();
+            //numCourses--;
+            count++;
+            //if(catalog[course] != nullptr)
+            //{
+            for(auto next : catalog[course])
+            {
+                prereq[next]--;
+                if(prereq[next] == 0)
+                    q.push(next);
+            }
+            //}
+
+        }
+        return numCourses == count;
+    }
+};
