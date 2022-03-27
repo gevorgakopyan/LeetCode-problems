d0ffb352c791dc3036f8f48d64f812fbdb913839
gevorgakopyan <88122544+gevorgakopyan@users.noreply.github.com>
2022-03-27 11:13:30 -0700

    Time: 1454 ms (41.14%), Space: 18.2 MB (44.06%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,15 +1,14 @@
 class Solution:
-    def twoSum(self, nums, i, res):
-        map = {}
-        #target = nums[i]
+    def twoSum(self, nums, i , res):
+        seen = {}
         j = i + 1
         while j < len(nums):
             complement = - nums[i] - nums[j]
-            if complement in map:
+            if complement in seen:
                 res.append([nums[i], nums[j], complement])
-                while j + 1 < len(nums) and nums[j] == nums[j + 1]:
-                   j += 1
-            map[nums[j]] = j
+                while j + 1 < len(nums) and nums[j] == nums[j+1]:
+                    j+=1
+            seen[nums[j]] = j
             j+=1
         return res
     
@@ -18,7 +17,7 @@ class Solution:
         nums.sort()
         for i in range(len(nums)):
             if nums[i] > 0:
-                break
-            if i == 0 or nums[i] != nums[i - 1]:
-                self.twoSum(nums,i, res)
+                break;
+            if i == 0 or nums[i-1] != nums[i]:
+                self.twoSum(nums, i , res)
         return res
