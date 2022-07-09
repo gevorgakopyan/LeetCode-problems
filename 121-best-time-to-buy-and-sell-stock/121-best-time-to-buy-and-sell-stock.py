9be9643b02a04f3942a26aced825aa72aeac8448
gevorgakopyan <88122544+gevorgakopyan@users.noreply.github.com>
2022-07-09 00:00:36 -0700

    Time: 2186 ms (8.91%), Space: 25 MB (85.43%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxProfit = 0
-        currMin = float('inf')
+        minim = 10000
         for i in range(len(prices)):
-            currDiff = prices[i] - currMin;
-            if prices[i] < currMin:
-                currMin = prices[i];
-                currDiff = prices[i] - currMin;
-            elif currDiff > maxProfit:
-                maxProfit = currDiff
+            if prices[i] < minim:
+                minim = prices[i]
+            elif prices[i] - minim > maxProfit:
+                maxProfit = prices[i] - minim
         return maxProfit
