fd949874b60d2bb004d477818e74b8394d56a592
gevorgakopyan <88122544+gevorgakopyan@users.noreply.github.com>
2022-07-12 22:14:25 -0700

    Time: 111 ms (64.91%), Space: 14 MB (68.06%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -2,32 +2,14 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        arr1, arr2 = [0] * 26, [0] * 26
-        for i in range(len(s1)):
-            arr1[ord(s1[i])- ord('a')] += 1
-            arr2[ord(s2[i])- ord('a')] += 1
-        
-        matches = 0
-        for i in range(26):
-            matches += (1 if arr1[i] == arr2[i] else 0)
-            
-        l = 0
-        for r in range(len(s1),len(s2)):
-            if matches == 26:
+        window = Counter(s2[:len(s1)])
+        s1Window = Counter(s1)
+        for ind,elem in enumerate(s2):
+            if window == s1Window:
                 return True
-            index = ord(s2[r]) - ord('a')
-            arr2[index] += 1
-            if arr1[index] == arr2[index]:
-                matches += 1
-            elif arr1[index] + 1 == arr2[index]:
-                matches -=1
-                
-            index = ord(s2[l]) - ord('a')
-            arr2[index] -= 1
-            if arr1[index] == arr2[index]:
-                matches += 1
-            elif arr1[index] - 1 == arr2[index]:
-                matches -=1
-                
-            l+=1
-        return matches == 26
+            if ind + len(s1) < len(s2):
+                window[elem] -= 1
+                window[s2[ind + len(s1)]] += 1
+            else:
+                return False
+        return False
